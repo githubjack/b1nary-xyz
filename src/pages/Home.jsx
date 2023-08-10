61eb8fd80be3727b393e2843afdc8225e5631148
Peter Ashman <ironserenity@mail.com>
2023-08-10 20:07:23 +0100

    styling uodate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 import '../styles.css';
 
+
+const HomePageHeader = () => {
+  <div className="header">
+  <header className="home-header">
+    <h1 className="h1">This is the Home Page test</h1>
+  </header>
+</div>
+}
 const Home = () => {
   return (
     <>
-      <div className="header">
-        <header className="home-header">
-          <h1 className="h1">This is the Home Page test</h1>
-        </header>
-      </div>
-
+    <HomePageHeader />
       <div className="main">
-        <p className="p">This is the main content of the Home Page</p>
-
-        <div>Test Div</div>
+        <div>Test Div for home page</div>
       </div>
 
-      <div className="footer"><h2>Footer</h2></div>
-      <div className="reachable"><h2>Social Media</h2></div>
+{/*       <div className="footer"><h2>Footer</h2></div>
+      <div className="reachable"><h2>Social Media</h2></div> */}
 
     </>
 
